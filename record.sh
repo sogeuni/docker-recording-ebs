5beaf0b68ce2f852f2f93c0777bf0e0afa510aa4
sogeuni <sogeuni@gmail.com>
2018-07-06 15:14:08 +0900

    새벽 2시 까지는 전날 날짜로 인식하도록 함

--- record.sh
+++ record.sh
@@ -7,17 +7,18 @@ PROGRAM_DASH=${PROGRAM// /-}
 RECORD_MINS=$(($2 * 60))
 
 # make directory
-REC_MONTH=`date +%y%m`
+REC_MONTH=`date -d '2 hour ago' +%y%m`
 RECORDING_ROOT=/recording/$PROGRAM/$REC_MONTH
 mkdir -p "$RECORDING_ROOT"
 
 # make record file name
-REC_DATE=`date +%Y%m%d`
-MP3_FILE_NAME=$REC_DATE"_"$PROGRAM_DASH.mp3
+REC_DATE=`date -d '2 hour ago' +%Y%m%d`
+MP3_FILE_NAME=$PROGRAM_DASH"_"$REC_DATE.mp3
 
 # recording
 TEMP_FLV=`mktemp -u`
-echo $TEMP_FLV
+echo "dump $TEMP_FLV"
 rtmpdump -r $STREAM -B $RECORD_MINS -o $TEMP_FLV -q
+echo "convert $MP3_FILE_NAME"
 ffmpeg -i $TEMP_FLV -ac 2 -ab 128 -af "volume=10dB" -vn -y -f mp3 "$RECORDING_ROOT/$MP3_FILE_NAME"
 rm $TEMP_FLV
